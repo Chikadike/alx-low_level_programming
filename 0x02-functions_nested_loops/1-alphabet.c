d48f7b1f4b785b416bac8205b6cd628f32999e46
Chikadike <dikechika87@gmail.com>
2022-10-19 05:58:00 -0700

    putcharoo

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main-prints the alphabet, in lowercase
+*Return:Always 0
+*/
+
+void print_alphabet('a'- 'z')
+{
+	char = 'x'
+	x = 'a'
+	for (x = 'a', x <= 'z', x++);
+	while (x <= z)
+	{		putchar("%d\n", x);
+		x++;
+	}
+	print_alphabet('a-z');	
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*main-Entry point
+*Return: Always 0
+*/
+Int_putchar(int x)
+{
+putchar(x);
+}
+
+main.h
+int _putchar(int x);
