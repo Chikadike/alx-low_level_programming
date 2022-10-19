c1b328b7bbb4f690938d28c4623e5ffa2476bd22
Chikadike <dikechika87@gmail.com>
2022-10-19 15:49:09 -0700

    putch

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
+
 /**
-*main-Entry point
-*Return:Return 0
+*main - Entry point
+*Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+char x [8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+int i;
+
+for (i =0; i < 8; i++)
+{
+_putchar(x[i]);
+}
+_putchar('\n')
 
 return (0);
 }
