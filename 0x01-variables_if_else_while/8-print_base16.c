0eba1aefdf4a06969353cc81b6b0e1c927223ddd
Chikadike <dikechika87@gmail.com>
2022-10-20 10:17:33 -0700

    90

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
 /**
-*
-*main-peogram entry point.
+*main-program entry point.
 *Return:0 no error,non zero if error.
 */
 int main(void)
 {
 int i;
 char hexvalues[] =
-"0123456789abcdef",
-for (i = 0; i < 16; i++)
+"0123456789abcdef"
+
+for (i = '0'; i <= 'f'; i++);
+
 {
 putchar(hexvalues[i]);
 }
 putchar('\n');
-result (0);
+
+return (0);
 }
 
