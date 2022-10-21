2ea4a2bbe70111883cb37fd4bd359412bdc6c33e
Chikadike <dikechika87@gmail.com>
2022-10-21 08:14:35 -0700

    c-program

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,63 +2,21 @@
 
 /**
 *print_alphabet_x10-print all alphabets in lowercase 10 times.
-Return:On success 1.
+*Return:On success 1.
 */
 void print_alphabet_x10(void)
 {
-char i, k;
+int i, k;
 
-while (n <10)
-
-for  (k = 0; k < 10; k++)
+for (k = 0; k < 10; k++)
 {
 
-for (i = 'a';i <= 'z'; i++);
+for (i = 'a'; i <= 'z'; i++)
 {
-	_putchar(i);
-	n++;
+_putchar(i);
+n++;
 }
-	_putchar('\n');
-}
-}
-#include "main.h"
-
-/**
-*print_alphabet_x10-print all alphabets in lowercase 10 times.
-Return:On success 1.
-*/
-void print_alphabet_x10(void)
-{
-char i, k;
-
-for  (k = 0; k < 10; k++)
-{
-
-for (i = 'a',i <= 'z'; i++)
-{
-	_putchar(i);
-}
-	_putchar('\n')
-}
-}
-#include "main.h"
-
-/**
-*print_alphabet_x10-print all alphabets in lowercase 10 times.
-Return:On success 1.
-*/
-void print_alphabet_x10(void)
-{
-char i, k;
-
-for  (k = 0; k < 10; k++)
-{
-
-for (i = 'a',i <= 'z'; i++)
-{
-	_putchar(i);
-}
-	_putchar('\n')
+_putchar('\n');
 }
 }
 
