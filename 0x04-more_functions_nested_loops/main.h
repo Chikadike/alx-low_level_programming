07c2d248862cf50556948e8d07d93663c3b5ebb8
Chikadike <dikechika87@gmail.com>
2022-10-21 15:20:14 -0700

    asd

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+*File - main.h
+*Author: Dike Chika
+*Desc: Header contains prototype of all functions
+*      Used in 0x04-more_functions_nested_loops directory
+*/
+
+int _putchar(char c):
+
+
+
+
+
+
+
+#endif
