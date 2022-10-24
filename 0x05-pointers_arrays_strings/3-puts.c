0083a751a1ae312ee43801d0cb2eb3ebfadd0d57
Chikadike <dikechika87@gmail.com>
2022-10-24 15:34:05 -0700

    put2

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -12,14 +12,14 @@ int count = 0;
 while (count >= 0)
 {
 if (str[count] == '\0')
-{
+	{
 
-_putchar('\n');
-break;
+	_putchar('\n');
+	break;
 
-}
-_putchar(str[count]);
-count++;
+	}
+	_putchar(str[count]);
+	count++;
 }
 }
 
