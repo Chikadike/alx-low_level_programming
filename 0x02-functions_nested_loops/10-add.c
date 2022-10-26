ade6d2dd232f99e61d7339c53e284e4acac76adc
Chikadike <dikechika87@gmail.com>
2022-10-26 06:40:11 -0700

    c-program

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+
+
+/**
+* add - Entry point
+* @x: The character to take input.
+* @y: The character to take input.
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately
+*/
+
+int add(int x, int y)
+{
+
+return (x + y);
+
+}
