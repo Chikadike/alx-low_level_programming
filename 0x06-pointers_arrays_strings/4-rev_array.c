591f16a8abfe95e23a3a534dad6d806cb292c9ce
Chikadike <dikechika87@gmail.com>
2022-10-27 16:05:38 -0700

    pointers and arrays

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* reverse_array - A function to reverse the array given
+* @a: the variable having the array
+* @n: the variable storing the size of the array
+* Return: it returns void.
+*/
+
+void reverse_array(int *a, int n)
+{
+
+int tmp, index;
+
+for (index = n - 1; index >= n / 2; index--)
+
+{
+
+tmp = a[n - 1 - index];
+
+a[n - 1 - index] = a[index];
+
+a[index] = tmp;
+
+}
+
+}
