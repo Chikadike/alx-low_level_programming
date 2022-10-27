3a22a55dbef5b6b9b386d3e03af33de7f2ecbe9f
Chikadike <dikechika87@gmail.com>
2022-10-27 14:19:06 -0700

    pointers and arrays

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,56 +7,31 @@
 * Return: returns str string in caps.
 */
 
-
 char *cap_string(char *str)
 {
-
 int index = 0;
-
 while (str[index])
-
 {
-
 while (!(str[index] >= 'a' && str[index] <= 'z'))
-
 index++;
-
 if (str[index - 1] == ' ' ||
-
 str[index - 1] == '\t' ||
-
 str[index - 1] == '\n' ||
-
 str[index - 1] == ',' ||
-
 str[index - 1] == ';' ||
-
 str[index - 1] == '.' ||
-
 str[index - 1] == '!' ||
-
 str[index - 1] == '?' ||
-
 str[index - 1] == '"' ||
-
 str[index - 1] == '(' ||
-
 str[index - 1] == ')' ||
-
 str[index - 1] == '{' ||
-
 str[index - 1] == '}' ||
-
 index == 0)
-
 str[index] -= 32;
-
 index++;
-
 }
-
 return (str);
-
 }
 
 
