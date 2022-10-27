61570afc1c18a06cba7e28944c887930049d2c7a
Chikadike <dikechika87@gmail.com>
2022-10-27 16:10:28 -0700

    pointers and arrays

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,13 +7,19 @@
 */
 
 void print_number(int n)
+
 {
+
 unsigned int num = n;
+
 if (n < 0)
+
 {
 
 _putchar('-');
+
 num = -num;
+
 }
 
 if ((num / 10) > 0)
@@ -23,4 +29,3 @@ print_number(num / 10);
 _putchar((num % 10) + '0');
 
 }
-
