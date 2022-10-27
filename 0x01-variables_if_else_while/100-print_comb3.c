cabd78715f346dc272bc6a07aa1b1adc97867fd0
Chikadike <dikechika87@gmail.com>
2022-10-27 15:44:01 -0700

    variables

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+int i, j;
+
+for (i = '0'; i < '9'; ++i)
+{
+
+for (j = i; j <= '8'; j++)
+
+{
+
+putchar(i);
+
+putchar(j + 1);
+
+if (i < '8')
+
+{
+putchar(',');
+
+putchar(' ');
+}
+
+}
+
+}
+putchar('\n');
+
+return (0);
+}
+
