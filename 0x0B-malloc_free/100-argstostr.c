9a5a0be25cf52efcf8e8099dbbc7b16ae3bb2454
Chikadike <dikechika87@gmail.com>
2022-11-09 16:30:52 -0800

    malloc

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,38 +10,23 @@
 */
 
 char **strtow(char *str)
-
 {
+int i, w, j, k, count, m, wordf;
+char **p;
+char *x;
 
-	int i, w, j, k, count, m, wordf;
-
-	char **p;
-
-	char *x;
-
-
-
-	w = 0;
-
-	j = 0;
-
-	i = 0;
-
-	count = 0;
-
-	if (*str == '\0' || str == NULL)
-
-		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
-
-	{
-
+w = 0;
+j = 0;
+i = 0;
+count = 0;
+if (*str == '\0' || str == NULL)
+return (NULL);
+for (i = 0; str[i] != '\0'; i++)
+{
 		if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
 
 			w++;
-
-	}
+}
 
 	p = (char **)malloc((w + 1) * sizeof(char *));
 
@@ -52,14 +37,9 @@ char **strtow(char *str)
 	for (wordf = 0; str[wordf] && j <= w; wordf++)
 
 	{
-
-		count = 0;
-
-		if (str[wordf] != ' ')
-
-		{
-
-			for (i = wordf ; str[i] != '\0'; i++)
+count = 0;
+if (str[wordf] != ' ')
+for (i = wordf ; str[i] != '\0'; i++)
 
 			{
 
