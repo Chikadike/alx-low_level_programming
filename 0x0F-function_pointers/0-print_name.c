2bda96e007dea57dcbd49e6a50efa88e0afa0351
Chikadike <dikechika87@gmail.com>
2022-11-17 07:44:16 -0800

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,13 @@
 #include "function_pointers.h"
-#include <stdio.h>
+
 
 /**
-*print_name- prints name using a function pointer.
-*@name: name to be printed
-*@f: function pointer
-*Return: nothing.
+*print_name - prints a name
+*@name:The name to be printed
+*@f: A pointer to a function that prints a name
 */
 
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
 if (name == NULL || f == NULL)
 return;
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,18 @@
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
 
+/*
+* File: function_pointers.h
+* AUth: Dike Chika
+* Desc: Header file containing prototypes for all functions
+*       used in the 0x0E-function_pointers directory.
+*/
+
+
+
+#include <stdlib.h>
+
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 
